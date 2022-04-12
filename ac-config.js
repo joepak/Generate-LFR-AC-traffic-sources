de2c07b7d8ea870e1e5d1c1bfc6487ab275e2e15
joepak <74565906+joepak@users.noreply.github.com>
2022-04-11 23:41:11 -0700

    Update ac-config.js

--- ac-config.js
+++ ac-config.js
@@ -2,8 +2,8 @@ const acConfig = {
   endpoint: 'https://osbasahpublisher-ac-asahuat.lfr.cloud/',
   projectId: 'asah0878884185e94d299b56779700af5d43',
   page: {
-    title: 'Home - Liferay',
-    url: 'http://liferay.local:8080/',
+    title: 'Home - Liferay DXP',
+    url: 'http://redwedding.com:8080/',
   },
 }
 
